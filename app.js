14a8554496ce9e6d3c0d6dcb774ce81d2f5b7824
David Smith <dbsmith.dbs83@gmail.com>
2016-06-14 14:54:27 -0700

    Added first question

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 'use strict';
 
-alert('Yeahhh!');
+var userName = prompt('Identify yourself, human.');
+alert('Greetings, ' + userName + '.  Welcome to David\'s about me page');
+
+var answer1 = prompt('Does David have a dog?').toLowerCase();
+if (answer1 === 'no' || answer1 === 'no.' || answer1 === 'n') {
+  alert('Correct.');
+} else {
+  alert('Incorrect.  David has no dogs at this time.');
+}
