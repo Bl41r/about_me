8534eb453c37d15424c17a1f4437fbbb9eb43c9a
Derek Hewitt <derekmhewitt@gmail.com>
2016-06-16 13:49:07 -0700

    pair programming revision one, question 6 and 7 done

--- app.js
+++ app.js
@@ -59,15 +59,16 @@ function playGuessingGame(num) {
   }
 }
 
-// Let's get their name
-var userName = prompt('Identify yourself, human.');
-if (userName == null || userName === '') {
-  //used == in case of undefined type since (null == undefined) is true
-  userName = 'Mr./Ms. too-cool-to-enter-a-name';
+//Let's get their name
+function getUserName(){
+  var userName = prompt('Identify yourself, human.');
+  if (userName == null || userName === '') {
+    //used == in case of undefined type since (null == undefined) is true
+    userName = 'Mr./Ms. too-cool-to-enter-a-name';
+  }
+  alert('Greetings, ' + userName + '.  Welcome to David\'s about me page');
+  console.log('userName: ' + userName.toString());
 }
-alert('Greetings, ' + userName + '.  Welcome to David\'s about me page');
-console.log('userName: ' + userName.toString());
-
 // --Initial questions
 // question, answer, and response arrays
 var qArray = ['Does David have a dog?', 'Does David play a musical instrument?', 'Does David enjoy meatloaf?', 'Does David prefer mac to pc?','Does David hope to be a software developer?'];
@@ -103,6 +104,62 @@ for (var q = 0; q < qArray.length; q++) {
   }
 }
 
+// --Array question 7
+// This uses a nested for loop, the i-loop for the 6 guesses, and
+// the j-loop for checking against each element in the array
+function question7(){
+  var answersArray = ['nc', 'il', 'north carolina', 'illinois'];
+  var arrayCorrect = false; //If they guess correctly, becomes true
+  alert('Let\'s play another game.  You guess what other state I have lived in.');
+  for (var i = 0; i < 6; i++) {
+    console.log('i = ' + i);
+    if (arrayCorrect === true) {
+      break; // Breaks from 'i-loop' if they got it right.
+    } else {
+      if (i > 0) {
+        alert('Nope!');
+      }
+      try {
+        var state = prompt('What is your guess?').toLowerCase();
+      }
+      catch(error) {
+        console.log('error with Array game input, empty string used');
+        state = '';
+      }
+      for (var j = 0; j < answersArray.length; j++) {
+        console.log('j = ' + j);
+        if (state === answersArray[j]) {  // if their guess was correct..
+          tally++;
+          alert('Nice Work!');
+          arrayCorrect = true;
+          break; // Breaks from 'j-loop'
+        }
+      }
+    }
+  }
+
+// This little do-da put the answersArray elements into a nice string separated by commas:
+  var answerString = '';
+  for (var k = 0; k < answersArray.length - 1; k++) {
+    answerString += answersArray[k] + ', ';
+  }
+  answerString += answersArray[answersArray.length - 1];
+  alert('Possible answers were: ' + answerString);
+  console.log('was array question correct? ' + arrayCorrect);
+}
+
+// --Final answers message
+function reportScore(){
+  if (tally > 2) {
+    alert('You got ' + tally + ' out of 7 correct.  Nice work, ' + userName + '.');
+  } else if (tally > 0){
+    alert('You only got ' + tally + ' out of 7.  Boo, ' + userName + '.');
+  } else {
+    alert('You did not get a single question correct.  Either you did this on purpose, or you have awesomely terrible luck!');
+  }
+}
+getUserName();
+
 // --This is where the guessing game begins
 var playGame = confirm('Shall we play a game?  How about global thermonuclear war?  Just kidding.  I am thinking of a number between 1 and 20.  What number is it?  You have 4 attempts.');
 if (playGame) {
@@ -110,55 +167,8 @@ if (playGame) {
 } else {
   alert('Well, you\'re no fun.');
 }
+question7();
+reportScore();
 
-// --Array question 7
-// This uses a nested for loop, the i-loop for the 6 guesses, and
-// the j-loop for checking against each element in the array
-var answersArray = ['nc', 'il', 'north carolina', 'illinois'];
-var arrayCorrect = false; //If they guess correctly, becomes true
-alert('Let\'s play another game.  You guess what other state I have lived in.');
-for (var i = 0; i < 6; i++) {
-  console.log('i = ' + i);
-  if (arrayCorrect === true) {
-    break; // Breaks from 'i-loop' if they got it right.
-  } else {
-    if (i > 0) {
-      alert('Nope!');
-    }
-    try {
-      var state = prompt('What is your guess?').toLowerCase();
-    }
-    catch(error) {
-      console.log('error with Array game input, empty string used');
-      state = '';
-    }
-    for (var j = 0; j < answersArray.length; j++) {
-      console.log('j = ' + j);
-      if (state === answersArray[j]) {  // if their guess was correct..
-        tally++;
-        alert('Nice Work!');
-        arrayCorrect = true;
-        break; // Breaks from 'j-loop'
-      }
-    }
-  }
-}
-// This little do-da put the answersArray elements into a nice string separated by commas:
-var answerString = '';
-for (var k = 0; k < answersArray.length - 1; k++) {
-  answerString += answersArray[k] + ', ';
-}
-answerString += answersArray[answersArray.length - 1];
-alert('Possible answers were: ' + answerString);
-console.log('was array question correct? ' + arrayCorrect);
 console.log('tally is now ' + tally);
 console.log('ze final tally: ' + tally.toString() + ' out of 7');
-
-// --Final answers message
-if (tally > 2) {
-  alert('You got ' + tally + ' out of 7 correct.  Nice work, ' + userName + '.');
-} else if (tally > 0){
-  alert('You only got ' + tally + ' out of 7.  Boo, ' + userName + '.');
-} else {
-  alert('You did not get a single question correct.  Either you did this on purpose, or you have awesomely terrible luck!');
-}
