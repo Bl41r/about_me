5422f52f549e4b3632d8252859fa7a26d47ae963
David Smith <dbsmith.dbs83@gmail.com>
2016-06-17 10:12:52 -0700

    JS organization, NaN case

--- app.js
+++ app.js
@@ -4,10 +4,11 @@
 // and a question with multiple correct answers stored into an array
 
 'use strict';
-
-var tally = 0;  // Global var for number of questions correct
+// Global Variables
+var tally = 0;  // number of questions correct
 var numToGuess = 19;  // number for the guessing game
 
+//Functions
 // This function uses logical control flow to play the guessing game.
 // num is the correct number the coder wants matched.
 function playGuessingGame(num) {
@@ -16,10 +17,11 @@ function playGuessingGame(num) {
   do {
     try {
       var guess = parseInt(prompt('What is your guess?'));
+      console.log('guess: ' + guess);
     }
     catch(error) {
+      console.log('error, guess of: ' + guess + 'set to empty string');
       guess = '';
-      console.log('error, guess set to empty string');
     }
     if (guess === num) {
       alert('Great, kid!  Don\'t get cocky.');
@@ -32,6 +34,9 @@ function playGuessingGame(num) {
     } else if (guess > num) {
       alert('Incorrect, too high.');
       console.log('high');
+    } else if (isNaN(guess)) {
+      alert('Please enter a valid number.  Try again.');
+      attempt--;
     } else {
       alert('I don\'t know what you said, but no.');
       console.log('strange input: ' + guess);
@@ -48,7 +53,7 @@ function playGuessingGame(num) {
 function namePrompt() {
   userName = prompt('Identify yourself, human.');
   if (userName == null || userName === '') {
-    //used == in case of undefined type since (null == undefined) is true
+    // used == in case of undefined type since (null == undefined) is true
     userName = 'Mr./Ms. too-cool-to-enter-a-name';
   }
   alert('Greetings, ' + userName + '.  Welcome to David\'s about me page');
@@ -69,7 +74,7 @@ function questions1thru5(){
     }
     catch(error) {
       answer = '';
-      console.log('error, answer set to empty string for question' + q);
+      console.log('error, answer set to empty string for question ' + q);
     }
     // Apply uniformity to compare with answers formatted in aArray
     if ((answer === 'yes') || (answer === 'y') || (answer === 'yes.')) {
@@ -78,7 +83,7 @@ function questions1thru5(){
       answer = 'N';
     } else {
       answer = 'U'; // U for unknown
-      alert('Please give a valid yes or no response');
+      alert('Please give a valid yes or no response.  Try again.');
       q--; // ask question again
     }
     // test the answer given vs. the answer array
@@ -126,7 +131,8 @@ function question7(){
       }
     }
   }
-// This little do-da puts the answersArray elements into a legible string separated by commas:
+// This little do-da puts the answersArray elements into a legible string separated by commas
+// and alerts the user to possible correct answers
   var answerString = '';
   for (var k = 0; k < answersArray.length - 1; k++) {
     answerString += answersArray[k] + ', ';
@@ -147,7 +153,7 @@ function reportScore(){
   }
 }
 
-// Main program Loop
+// Main program loop
 var userName = namePrompt();
 questions1thru5();
 //--This is where the guessing game begins
@@ -157,6 +163,7 @@ if (playGame) {
 } else {
   alert('Well, you\'re no fun.');
 }
+//--
 question7();
 reportScore();
 console.log('ze final tally: ' + tally.toString() + ' out of 7');
